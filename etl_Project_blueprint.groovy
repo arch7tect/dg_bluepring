c0fcccfd7ff1ba03b9866f46b8b07f3c75e8d686
Oleg Orlov <orlov@neoflex.ru>
2019-04-11 15:00:41 +0300

    orlov: test commit

--- etl_Project_blueprint.groovy
+++ etl_Project_blueprint.groovy
@@ -1,3 +1,4 @@
+
 import ru.neoflex.meta.utils.Context
 def session = Context.current.txSession
 def entity = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult() 
