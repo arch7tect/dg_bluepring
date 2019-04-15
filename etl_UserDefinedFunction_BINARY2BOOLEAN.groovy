1842d67c375c609b205397f682c5d12c95f7504e
Oleg Orlov <orlov@neoflex.ru>
2019-04-15 13:15:05 +0300

    arch7tect: test commit 2

--- etl_UserDefinedFunction_BINARY2BOOLEAN.groovy
+++ etl_UserDefinedFunction_BINARY2BOOLEAN.groovy
@@ -1,5 +1,5 @@
 import ru.neoflex.meta.utils.Context
 def session = Context.current.txSession
 def entity = session.createQuery("from etl.UserDefinedFunction where name = :name").setParameter("name", "BINARY2BOOLEAN").uniqueResult() 
-entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult()
+entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint_git").uniqueResult()
 Context.current.commit()
--- rt_LivyServer_LivyOnCloud2.groovy
+++
@@ -1,5 +0,0 @@
-import ru.neoflex.meta.utils.Context
-def session = Context.current.txSession
-def entity = session.createQuery("from rt.LivyServer where name = :name").setParameter("name", "LivyOnCloud2").uniqueResult() 
-entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "simpleTest").uniqueResult()
-Context.current.commit()
