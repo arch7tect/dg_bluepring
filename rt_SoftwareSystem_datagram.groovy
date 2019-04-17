e0138e186c2a9d4cb99f6c273424f37ea8667774
Oleg Orlov <orlov@neoflex.ru>
2019-04-17 11:04:21 +0300

    arch7tect: test commit 2

--- rt_SoftwareSystem_datagram.groovy
+++ rt_SoftwareSystem_datagram.groovy
@@ -1,7 +1,7 @@
 import ru.neoflex.meta.utils.Context
 def session = Context.current.txSession
 def entity = session.createQuery("from rt.SoftwareSystem where name = :name").setParameter("name", "datagram").uniqueResult() 
-entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "sandbox").uniqueResult()
-entity.scheme = session.createQuery("from rel.Scheme where name = :name").setParameter("name", "datagram").uniqueResult()
+entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint_git").uniqueResult()
+entity.scheme = session.createQuery("from rel.Scheme where name = :name").setParameter("name", "public_at_PostgresDatagram").uniqueResult()
 entity.defaultDeployment = session.createQuery("from rt.Deployment where name = :name").setParameter("name", "DatagramOnLocal").uniqueResult()
 Context.current.commit()
