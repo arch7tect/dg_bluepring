fe2c47590193227e903ffab0f3b9e469988d5bb2
jenkins <jenkins@cloud.neoflex.ru>
2019-05-14 13:08:51 +0300

    mtugushev: Notebook

--- rt_LivyServer_ih921081.vds.myihor.ru.groovy
+++
@@ -1,5 +0,0 @@
-import ru.neoflex.meta.utils.Context
-def session = Context.current.txSession
-def entity = session.createQuery("from rt.LivyServer where name = :name").setParameter("name", "ih921081.vds.myihor.ru").uniqueResult() 
-entity.project = null
-Context.current.commit()
