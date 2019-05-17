03b65a6baec12f322973f43e7b1980e2d51a7922
jenkins <jenkins@cloud.neoflex.ru>
2019-05-17 11:19:47 +0300

    mtugushev: Notebook

--- sse_Dataset_ModelingPD_dictEconomicSectors.groovy
+++ sse_Dataset_ModelingPD_dictEconomicSectors.groovy
@@ -4,6 +4,6 @@ def entity = session.createQuery("from sse.Dataset where name = :name").setParam
 entity.datasets.clear()
 entity.workspace = session.createQuery("from sse.AnalyticWorkspace where name = :name").setParameter("name", "ModelingPD").uniqueResult()
 entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint_git").uniqueResult()
-entity.datasets.add(session.createQuery("from sse.HiveDataset where name = :name").setParameter("name", "ModelingPD_Sectores_de_Economia").uniqueResult())
 entity.datasets.add(session.createQuery("from sse.HiveDataset where name = :name").setParameter("name", "ModelingPD_GE_EconomicActivityMissing").uniqueResult())
+entity.datasets.add(session.createQuery("from sse.HiveDataset where name = :name").setParameter("name", "ModelingPD_Sectores_de_Economia").uniqueResult())
 Context.current.commit()
