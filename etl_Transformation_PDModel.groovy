8958a62986b5632b6a9094a0dd9c8b4348b90b71
orlov <orlov@orlov-tp7.neoflex.ru>
2019-05-21 18:37:59 +0300

    admin: Notebook

--- etl_Transformation_PDModel.groovy
+++ etl_Transformation_PDModel.groovy
@@ -3,12 +3,12 @@ def session = Context.current.txSession
 def entity = session.createQuery("from etl.Transformation where name = :name").setParameter("name", "PDModel").uniqueResult() 
 entity.userDefinedFunctions.clear()
 entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint_git").uniqueResult()
-entity.targets.find {it.name == "Local_16"}.context = null
-entity.targets.find {it.name == "Local_13"}.context = null
-entity.targets.find {it.name == "Local_10"}.context = null
-entity.targets.find {it.name == "Local_7"}.context = null
-entity.targets.find {it.name == "Local_4"}.context = null
 entity.targets.find {it.name == "Local_1"}.context = null
+entity.targets.find {it.name == "Local_4"}.context = null
+entity.targets.find {it.name == "Local_7"}.context = null
+entity.targets.find {it.name == "Local_10"}.context = null
+entity.targets.find {it.name == "Local_13"}.context = null
+entity.targets.find {it.name == "Local_16"}.context = null
 entity.sources.find {it.name == "DictPortfolio"}.context = null
 entity.sources.find {it.name == "GE_EconomicActivityMissing"}.context = null
 entity.sources.find {it.name == "rcdataAccounts"}.context = null
